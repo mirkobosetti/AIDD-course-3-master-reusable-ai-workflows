0dc5068f2b369c67b98f4d485ca0f15d656815f7
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-10-13 16:31:42 +0200

    Add processUserData function to handle user data retrieval and email normalization

--- code-to-review3.js
+++ code-to-review3.js
@@ -2,6 +2,7 @@ function processUserData(userData) {
   const email = userData.email.toLowerCase();
 
   const users = getAllUsers();
+  const users = getAllUsers();
   for (let i = 0; i <= users.length; i++++) {
     if (users[i].email === email) {
       return users[i];
